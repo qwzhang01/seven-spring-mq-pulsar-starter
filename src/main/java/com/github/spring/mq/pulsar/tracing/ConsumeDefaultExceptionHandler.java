c897657bad7d9eda9f80107453ca582bf8034cd6
avinzhang <avinzhang@tencent.com>
2025-09-30 21:37:57 +0800

    完善细节

--- src/main/java/com/github/spring/mq/pulsar/tracing/ConsumeDefaultExceptionHandler.java
+++ src/main/java/com/github/spring/mq/pulsar/tracing/ConsumeDefaultExceptionHandler.java
@@ -5,10 +5,12 @@ import com.github.spring.mq.pulsar.annotation.ConsumerExceptionResponse;
 import com.github.spring.mq.pulsar.domain.ConsumerExceptionResponseAction;
 import com.github.spring.mq.pulsar.exception.JacksonException;
 import com.github.spring.mq.pulsar.exception.PulsarConsumerLatterException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Default exception handler for message consumption
- * 
+ *
  * <p>Provides default exception handling strategies for common exceptions
  * that may occur during message consumption. This handler defines how
  * different types of exceptions should be handled and what response
@@ -18,22 +20,23 @@ import com.github.spring.mq.pulsar.exception.PulsarConsumerLatterException;
  * @since 1.0.0
  */
 public class ConsumeDefaultExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ConsumeDefaultExceptionHandler.class);
 
     @ConsumerExceptionHandler({UnsupportedOperationException.class, JacksonException.class})
     @ConsumerExceptionResponse(ConsumerExceptionResponseAction.ACK)
     public void unsupportedOperationException(Exception exception) {
-
+        logger.error("Error processing message", exception);
     }
 
     @ConsumerExceptionHandler(PulsarConsumerLatterException.class)
     @ConsumerExceptionResponse(ConsumerExceptionResponseAction.RECONSUME_LATER)
     public void pulsarConsumerLatterException(PulsarConsumerLatterException exception) {
-
+        logger.error("Error processing message", exception);
     }
 
     @ConsumerExceptionHandler(Exception.class)
     @ConsumerExceptionResponse(ConsumerExceptionResponseAction.NACK)
     public void exception(Exception exception) {
-
+        logger.error("Error processing message", exception);
     }
 }
